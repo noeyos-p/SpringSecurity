7fa0ac8880773a6fea96c11d268e7eee8e162541
noeyos-p <krapnoeyos@gmail.com>
2025-09-03 12:57:47 +0900

    로그인 페이지 완성

--- src/main/java/com/my/securityTest/controller/MainController.java
+++ src/main/java/com/my/securityTest/controller/MainController.java
@@ -1,17 +1,27 @@
 package com.my.securityTest.controller;
 
+import org.hibernate.grammars.hql.HqlParser;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        // 현재 로그인 된 사용자 정보를 확인
+        String username = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+        model.addAttribute("user", username);
         return "main";
     }
 
     @GetMapping("/admin")
     public String adminPage() {
+
         return "admin";
     }
 }
--- src/main/java/com/my/securityTest/repository/UserRepository.java
+++ src/main/java/com/my/securityTest/repository/UserRepository.java
@@ -7,4 +7,6 @@ public interface UserRepository extends JpaRepository <UserEntity, Long> {
     // 아이디가 존재하는지 확인하는 쿼리 메서드 만들기
     boolean existsByUsername(String username);
 
+    // username 으로 검색 한 결과 리턴
+    UserEntity findByUsername(String username);
 }
